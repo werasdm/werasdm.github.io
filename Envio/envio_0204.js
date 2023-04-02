5339463845c7f8d86fb6fdf5a13029b289885b0f
werasdm <vinicius.verasdemoraes@gmail.com>
2023-04-02 20:00:12 -0300

    a

---
+++ Envio/envio_0204.js
@@ -0,0 +1,64 @@
+// This is a test of the data sharing capabilities of p5LiveMedia webrtc library and associated service.
+// Open this sketch up 2 times to send data back and forth
+let input;
+let img;
+let x = 0;
+let y = 0;
+let envio;
+let button;
+let dataToSend;
+
+let p5lm;
+
+function setup() {
+  let myCanvas = createCanvas(500, 500);
+  input = createFileInput(handleFile);
+  input.position(0, 0);
+  button = createButton('enviar');
+  button.position(0, 30);
+  button.mousePressed(enviarImg);
+  p5lm = new p5LiveMedia(this, "DATA", null, "uninhobananamovelvimiv");
+  p5lm.on('data', gotData);
+  p5lm.on('disconnect', gotDisconnect);
+}
+
+function draw() {
+  background(220);
+   if (img) {
+    imageMode(CENTER);
+    image(img, 250, 250);
+  }
+}
+
+function gotDisconnect(id) {
+  print(id + ": disconnected");
+}
+
+function gotData(data, id) {
+  print(id + ":" + data);
+  
+  // If it is JSON, parse it
+  let d = JSON.parse(data);
+  x = d.x;
+  y = d.y;
+}
+
+
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = createImg(file.data, '');
+    envio = file;
+    img.hide();
+  } else {
+    img = null;
+  }
+  dataToSend = {a: file.data, b: file.name};
+}
+
+function enviarImg() {
+  
+  // Have to send string
+  p5lm.send(JSON.stringify(dataToSend));
+  
+}
---
+++ Recibo/quardar_0204.js
@@ -0,0 +1,38 @@
+// This is a test of the data sharing capabilities of p5LiveMedia webrtc library and associated service.
+// Open this sketch up 2 times to send data back and forth
+
+let x = 0;
+let y = 10;
+let img;
+let envio;
+
+let p5lm;
+
+function setup() {
+  p5lm = new p5LiveMedia(this, "DATA", null, "uninhobananamovelvimiv");
+  p5lm.on('data', gotData);
+  p5lm.on('disconnect', gotDisconnect);
+}
+
+function draw() {
+  if (img) {
+    image(img, 0, 0, width, height);
+    img.save('photo', 'png');
+    noLoop();
+  }
+}
+
+function gotDisconnect(id) {
+  print(id + ": disconnected");
+}
+
+function gotData(data, id) {
+  print(id + ":" + "recebido");
+  
+  // If it is JSON, parse it
+  let d = JSON.parse(data);
+  img = loadImage(d.a, '');
+  img.hide();
+  x++;
+  //save(img, d.b + ".png");
+}
