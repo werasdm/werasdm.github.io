bdd93c86ff15cd1428ba14fd826a5d2b2c78ce57
werasdm <vinicius.verasdemoraes@gmail.com>
2023-04-03 20:49:59 -0300

    redi

--- Envio/envio_0204.js
+++ Envio/envio_0204.js
@@ -6,12 +6,17 @@ let comparabotao = true;
 let mudancabotao = false;
 let envio;
 let button;
-let dataToSend;
+
+
+//redimencionar a imagem
+let picV;
+let picH;
+let w, h;
 
 let p5lm;
 
 function setup() {
-  let myCanvas = createCanvas(500, 500);
+  let myCanvas = createCanvas(350, 350);
   input = createFileInput(handleFile);
   input.position(0, 0);
   button = createButton('enviar');
@@ -19,17 +24,34 @@ function setup() {
   button.mousePressed(function(){
     mudancabotao = !mudancabotao;
   });
-  p5lm = new p5LiveMedia(this, "DATA", null, "uninhobananamovelvimiv");
+  p5lm = new p5LiveMedia(this, "DATA", null, "amorinhabjork");
   p5lm.on('data', gotData);
   p5lm.on('disconnect', gotDisconnect);
 }
 
 function draw() {
   background(220);
-   if (img) {
+  if (img) {
     imageMode(CENTER);
-    image(img, 250, 250);
-  }
+    
+    //Redimencionar a imagem  
+    // Vertical
+    if (img.height > img.width) {
+      hRatio = height / img.height;
+      h = img.height * hRatio;
+      w = img.width * hRatio;
+      image(img, width / 2, height / 2, w, h);
+    // Horizontal
+    } else if (img.width > img.height) {
+    let wRatio = width / img.width;
+    w = img.width * wRatio;
+    h = img.height * wRatio;
+    image(img, width / 2, height / 2, w, h);
+    // 1:1
+    } else {
+    image(img, width/2, height/2, width, height);
+    }
+   }
     if(mudancabotao == comparabotao){
     enviarImg();
     comparabotao = !comparabotao;
@@ -58,11 +80,12 @@ function handleFile(file) {
   } else {
     img = null;
   }
-  dataToSend = {a: file.data, b: file.name};
 }
 
 function enviarImg() {
   // Have to send string
+  let dataToSend = { a: canvas.toDataURL('image/png')};
+  print(dataToSend);
   print("enviou");
   p5lm.send(JSON.stringify(dataToSend));
   print("enviou2");
